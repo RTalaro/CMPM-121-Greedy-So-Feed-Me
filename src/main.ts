74c36e7e21573f865120c92aac8a0c3e816e67fb
RTalaro <146395601+RTalaro@users.noreply.github.com>
2025-10-08 15:56:55 +0000

    add working counter (step 2 complete)

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,18 @@
 import exampleIconUrl from "./noun-paperclip-7598668-00449F.png";
 import "./style.css";
 
+let age: number = 1;
+
 document.body.innerHTML = `
   <p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>
+  <div>Age: <span id = "age">${age}</span></div>
   <button id = "clicker">Happy Birthday!🎉</button>
-  `;
+`;
+
+const button = document.getElementById("clicker")!;
+const counter = document.getElementById("age")!;
+
+button.addEventListener("click", () => {
+  age = age + 1;
+  counter.textContent = age.toString();
+});
